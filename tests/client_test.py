8c0f3ad732463ee0af6415dbd263faa80a6d582a
alina.gryaznova <alina.gryaznova@sigmaukraine.com>
2017-02-15 17:23:34 +0200

    adds tests with requests_mock

--- tests/client_test.py
+++ tests/client_test.py
@@ -1,12 +1,16 @@
 import so_client
-import pytest_mock
+import requests_mock
 
 tag = 'python'
 
 
-# def test_returns_tags_inside_items_of_response(mocker):
-    # mocker.patch.object(response, 'json', return_value={'foo'})
-    # requests_mock = mocker.patch('requests.get')
-    # requests_mock.return_value = {}
-    # result = so_client.get_questions_by_tag(tag)
-    # assert result == ''
+def test_returns_tags_inside_items_of_response():
+    current_tag = 'python'
+    with requests_mock.mock() as m:
+        item1_tags = ['tag1', 'tag2']
+        item2_tags = ['tag3', 'tag4']
+        json_mock = {'items': [{'foo':'1', 'tags': item1_tags}, {'foo':'2', 'tags': item2_tags}]}
+        m.get('https://api.stackexchange.com/2.2/questions?site=stackoverflow&tagged=%s'%current_tag, json=json_mock)
+        result = so_client.get_questions_by_tag(current_tag)
+        assert result == [item1_tags, item2_tags]
+
