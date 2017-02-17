42bf9ff07baed4b92ad183031764e7978f8e4674
alina.gryaznova <alina.gryaznova@sigmaukraine.com>
2017-02-17 18:28:45 +0200

    adds basic logging and renames variable

--- so_client/__init__.py
+++ so_client/__init__.py
@@ -1,3 +1,8 @@
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(levelname)s %(asctime)s %(message)s',
+                    datefmt='%d-%m-%Y %H:%M:%S')
+
 from .client import (get_questions_by_tag)
 from .statistics import (
     get_top_tags,
--- so_client/client.py
+++ so_client/client.py
@@ -1,18 +1,18 @@
+import logging
 import concurrent.futures as cf
 import requests_futures.sessions as rf
 
 
 def get_questions_by_tag(tags):
     url = 'https://api.stackexchange.com/2.2/questions?site=stackoverflow&tagged={}'
+    logging.info('Requesting information about %s tags', len(tags))
     session = rf.FuturesSession()
     futures = [session.get(url.format(tag)) for tag in tags]
-    tags = []
+    found_tags = []
     for future in cf.as_completed(futures):
         response = future.result()
         json = response.json()
         items = json['items']
         for item in items:
-            tags.append(item['tags'])
-    return tags
-
-
+            found_tags.append(item['tags'])
+    return found_tags
